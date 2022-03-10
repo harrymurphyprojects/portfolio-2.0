901e29d154bf30f6b567099c782f93b76422f4cf
harrymurphy1534 <harryjmurphy1997@gmail.com>
2022-03-10 17:40:25 +0000

    profile-css

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,13 @@ function Profile() {
   return (
     <>
       <div className="container d-flex justify-content-evenly p-5 bg-light border">
-        <img src="https://media-exp1.licdn.com/dms/image/C5603AQENSdsEVlXo4Q/profile-displayphoto-shrink_800_800/0/1550567356040?e=1652313600&v=beta&t=uTOOk7L9T9um-fcvC02tLgtPJTMxwqKkZ5l1jMR5Su8"
-          className="rounded-circle">
+        <img src="https://media-exp1.licdn.com/dms/image/C4D03AQHeOXY5CZu1-g/profile-displayphoto-shrink_800_800/0/1646933286274?e=1652313600&v=beta&t=mdKy2XXGMsH_dypRv4Arusiao5cB92CT7YDVS3aXSgA"
+          className="img-fluid rounded-circle picture">
         
         </img>
         <div className="container text-end">
           <h1 className="display-3 pt-2 justify-text-end" >Harry Murphy</h1>
-          <h2 className="display-6 pt-2 text-muted" >A Junior Software Engineer</h2>
+          <h2 className="display-6 pt-2 text-muted" >Junior Software Engineer</h2>
         </div>
       </div>
     </>
