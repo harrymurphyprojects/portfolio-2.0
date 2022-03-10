0666373048c5e5d78689ba12aff8902c1158a75b
harrymurphy1534 <harryjmurphy1997@gmail.com>
2022-03-10 12:45:48 +0000

    nav-hover

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,27 +5,27 @@ function Navbar() {
   return (
     <nav className="p-2">
       <ul className="nav nav-pills justify-content-evenly">
-        <li className="text-decoration-none nav-item ">
+        <li className="btn">
           <a href="#about" className="nav-link active">About Me</a>
         </li>
-        <li className="nav-item">
+        <li className="btn">
           <a href="#skills" className="nav-link active">Skills</a>
         </li>
-        <li className="nav-item">
+        <li className="btn">
           <a href="#projects" className="nav-link active">Projects</a>
         </li>
-        <li className="nav-item">
+        <li className="btn">
           <a href="#interests" className="nav-link active">Interests</a>
         </li>
         <li className="justify-content-right">
           <a className="btn" href="https://www.linkedin.com/in/harry-murphy-projects">
-            <LinkedIn fontSize="Large" />
+            <LinkedIn className="icon" fontSize="Large" />
           </a>
           <a className="btn" href="https://github.com/harrymurphyprojects">
-            <GitHub fontSize="Large" />
+            <GitHub className="icon" fontSize="Large" />
           </a> 
           <a className="btn" href="mailto:harryjmurphy1997@gmail.com">
-            <Email fontSize="Large" />
+            <Email className="icon" fontSize="Large" />
           </a>
         </li>
       </ul>
